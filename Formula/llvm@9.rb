275b04aebadec11600e0002e19f74fb678ed0a3b
Michael Cho <cho-m@tuta.io>
2021-09-07 14:26:33 -0700

    llvm@9: use elfutils on Linux

--- Formula/llvm@9.rb
+++ Formula/llvm@9.rb
@@ -5,7 +5,7 @@ class LlvmAT9 < Formula
   sha256 "00a1ee1f389f81e9979f3a640a01c431b3021de0d42278f6508391a2f0b81c9a"
   # The LLVM Project is under the Apache License v2.0 with LLVM Exceptions
   license "Apache-2.0"
-  revision 2
+  revision 3
 
   bottle do
     rebuild 2
@@ -34,7 +34,7 @@ class LlvmAT9 < Formula
   on_linux do
     depends_on "glibc" if Formula["glibc"].any_version_installed?
     depends_on "binutils" # needed for gold and strip
-    depends_on "libelf" # openmp requires <gelf.h>
+    depends_on "elfutils" # openmp requires <gelf.h>
     depends_on "python@3.8"
   end
 
