7285fae98f75545ee35736de904fec1d27f2fb6f
Michael Cho <michael@michaelcho.dev>
2024-11-27 20:33:56 -0500

    zlib-ng-compat 2.2.2 (new formula)

---
+++ Formula/z/zlib-ng-compat.rb
@@ -0,0 +1,39 @@
+class ZlibNgCompat < Formula
+  desc "Zlib replacement with optimizations for next generation systems"
+  homepage "https://github.com/zlib-ng/zlib-ng"
+  url "https://github.com/zlib-ng/zlib-ng/archive/refs/tags/2.2.2.tar.gz"
+  sha256 "fcb41dd59a3f17002aeb1bb21f04696c9b721404890bb945c5ab39d2cb69654c"
+  license "Zlib"
+  head "https://github.com/zlib-ng/zlib-ng.git", branch: "develop"
+
+  livecheck do
+    formula "zlib-ng"
+  end
+
+  keg_only :shadowed_by_macos, "macOS provides zlib"
+
+  on_linux do
+    keg_only "it conflicts with zlib"
+  end
+
+  # https://zlib.net/zlib_how.html
+  resource "homebrew-test_artifact" do
+    url "https://zlib.net/zpipe.c"
+    sha256 "68140a82582ede938159630bca0fb13a93b4bf1cb2e85b08943c26242cf8f3a6"
+  end
+
+  def install
+    ENV.runtime_cpu_detection
+    system "./configure", "--prefix=#{prefix}", "--without-new-strategies", "--zlib-compat"
+    system "make", "install"
+  end
+
+  test do
+    testpath.install resource("homebrew-test_artifact")
+    system ENV.cc, "zpipe.c", "-I#{include}", lib/shared_library("libz"), "-o", "zpipe"
+
+    content = "Hello, Homebrew!"
+    compressed = pipe_output("./zpipe", content)
+    assert_equal content, pipe_output("./zpipe -d", compressed)
+  end
+end
